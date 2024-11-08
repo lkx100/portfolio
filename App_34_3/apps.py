e63b8185722dfe3b327911254daa77fa234cd047
lkx100 <lk5999950@gmail.com>
2024-11-08 11:39:31 +0530

    Initial Commit

---
+++ App_34_3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class App343Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'App_34_3'
---
+++ static/js/script.js
@@ -0,0 +1,3 @@
+function toggleSection(id) {
+    $('#' + id).collapse('toggle');
+}
